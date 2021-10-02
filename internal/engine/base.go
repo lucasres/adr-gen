36b02d651bb39114c3f1a8b82adc53cb371e8667
lucasres <lucasresone@gmail.com>
2021-10-02 14:23:22 -0300

    Add base engine interface

---
+++ internal/engine/base.go
@@ -0,0 +1,19 @@
+package engine
+
+type ADRHit struct {
+	Description string
+	ID          string
+}
+
+type ContentParsed struct {
+	RawContent  string
+	ByteContent []byte
+}
+
+type Engine interface {
+	PreProcess(content []byte) ([]byte, error)
+	Parse([]byte) (*ContentParsed, error)
+	Analize(content ContentParsed) (map[string]ADRHit, error)
+	Output(hits map[string]ADRHit) error
+	Run(content []byte) error
+}

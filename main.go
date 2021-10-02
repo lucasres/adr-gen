5c5b4bae19ac59ab305da997aec3d5d4e6a79659
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2021-10-01 23:17:06 -0300

    Implemented root command

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "adr-generator",
+		Short: "ADR Generator",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}

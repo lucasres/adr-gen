c835ac4e700b22fdfcd94a48642c836069b7bce6
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2021-10-02 21:46:03 -0300

    Implemented get pages action for Confluence service

--- pkg/service/confluence/page.go
+++ pkg/service/confluence/page.go
@@ -27,6 +27,22 @@ type PageResult struct {
 	types.IdentifiableEntity
 }
 
+type GetPagesInput struct {
+	SpaceKey *string
+	Title    *string
+	Type     *string
+	Status   *string
+	Start    *uint
+	Limit    *uint
+}
+
+type PageListResult struct {
+	Limit int          `json:"limit"`
+	Size  int          `json:"size"`
+	Start int          `json:"start"`
+	Pages []PageResult `json:"results"`
+}
+
 func (c *Client) CreatePage(ctx context.Context, page *CreatePageInput) (*PageResult, error) {
 	body := &bytes.Buffer{}
 	if err := json.NewEncoder(body).Encode(page); err != nil {
@@ -56,6 +72,63 @@ func (c *Client) CreatePage(ctx context.Context, page *CreatePageInput) (*PageRe
 	var result *PageResult
 	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
 		return nil, fmt.Errorf("can't decode create page response: %w", err)
+	}
+
+	return result, nil
+}
+
+// GetPages search for pages in confluence
+// https://docs.atlassian.com/ConfluenceServer/rest/7.11.6/#api/content-getContent
+func (c *Client) GetPages(ctx context.Context, input *GetPagesInput) (*PageListResult, error) {
+	req, err := c.createRequest(ctx, http.MethodGet, "content", nil)
+	if err != nil {
+		return nil, fmt.Errorf("can't create get pages request: %w", err)
+	}
+
+	q := req.URL.Query()
+
+	if input.SpaceKey != nil {
+		q.Add("spaceKey", *input.SpaceKey)
+	}
+
+	if input.Title != nil {
+		q.Add("title", *input.Title)
+	}
+
+	if input.Type != nil {
+		q.Add("type", *input.Type)
+	}
+
+	if input.Status != nil {
+		q.Add("status", *input.Status)
+	}
+
+	if input.Limit != nil {
+		q.Add("limit", fmt.Sprint(*input.Limit))
+	}
+
+	if input.Start != nil {
+		q.Add("start", fmt.Sprint(*input.Start))
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("can't request get pages: %w", err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(
+			"confluence don't return HTTP Status Code 200. Returned: [%d] - \"%s\"",
+			res.StatusCode,
+			res.Body,
+		)
+	}
+
+	var result *PageListResult
+	if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("can't decode get pages response: %w", err)
 	}
 
 	return result, nil

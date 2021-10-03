5bebe2f5c2a888c55d29c21919a87846649d9021
unknown <lucasresone@gmail.com>
2021-10-03 20:02:14 -0300

    Add metodo de analise para o sengine

--- internal/engine/sengine.go
+++ internal/engine/sengine.go
@@ -2,14 +2,33 @@ package engine
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
+	"strings"
 )
 
 type Sengine struct{}
 
 func (e *Sengine) Analize(c ContentParsed) (map[string]ADRHit, error) {
+	result := map[string]ADRHit{}
 
-	return nil, nil
+	hitRegex := regexp.MustCompile(`((\/\/|#)\s@ADR-\d):.*\n`)
+	hits := hitRegex.FindAll(c.ByteContent, -1)
+
+	for _, hit := range hits {
+		// limpa o // @
+		regexClear := regexp.MustCompile(`// @`)
+		hit = regexClear.ReplaceAll(hit, []byte(""))
+		//@todo: pode ser melhorado pegando por grupos direto do regex
+		// ADR-x : Lorem ipsun
+		splited := strings.Split(string(hit), ":")
+		result[splited[0]] = ADRHit{
+			ID:          splited[0],
+			Description: splited[1],
+		}
+	}
+
+	return result, nil
 }
 
 func (e *Sengine) Parse(content []byte) (*ContentParsed, error) {
@@ -20,14 +39,8 @@ func (e *Sengine) Parse(content []byte) (*ContentParsed, error) {
 }
 
 func (e *Sengine) PreProcess(content []byte) ([]byte, error) {
-	// remove end line for ;
-	regexEndLine := regexp.MustCompile(`\n`)
-	contentWithSemicolon := regexEndLine.ReplaceAll([]byte(content), []byte(";"))
-	// remove 2 spaces or more
-	regexTwoSpaces := regexp.MustCompile(`\s{2,}`)
-	contentWithSingleSpaces := regexTwoSpaces.ReplaceAll(contentWithSemicolon, []byte(""))
-
-	return contentWithSingleSpaces, nil
+	// @todo precisa ser refatorado, estava bugando o codigo
+	return content, nil
 }
 
 func (e *Sengine) Output(hits map[string]ADRHit) error {
@@ -41,10 +54,21 @@ func (e *Sengine) Run(content []byte) error {
 		return err
 	}
 
-	_, err = e.Parse(processed)
+	parsed, err := e.Parse(processed)
 
 	if err != nil {
 		return err
+	}
+
+	hits, err := e.Analize(*parsed)
+
+	if err != nil {
+		return err
+	}
+
+	for a, b := range hits {
+		fmt.Println(a)
+		fmt.Println(b.Description, b.ID)
 	}
 
 	return errors.New("must be not implemented")
